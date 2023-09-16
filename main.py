dae3a2acdb35dd8fd1b4a12e069a03d9e03a6aa8
hernandovela <hdo.vela@gmail.com>
2023-09-16 14:51:31 -0500

    subir archivos

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+nameApi = '/api/'
+
+@app.route(nameApi)
+def usuario():
+    return jsonify({"usaurio":"get"})
+
+@app.route(nameApi+'crear',methods=["POST"])
+def usuarioCrear():
+    data=request.get_json()
+    return jsonify(data)
+
+@app.route(nameApi+'actualizar',methods=["PUT"])
+def usuarioActaulizar():
+   return jsonify({"usaurio":"actualizar"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
